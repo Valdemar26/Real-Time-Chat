6d1fc135f5b24099c2006e3a3cde3372cd04281a
Valdemar26 <z_valdemar@ukr.net>
2016-04-29 17:05:16 +0300

    comment

---
+++ server.js
@@ -0,0 +1,44 @@
+var mongo = require('mongodb').MongoClient,
+    client = require('socket.io').listen(8080).sockets;
+
+mongo.connect('mongodb://127.0.0.1/chat', function(err, db) {
+    if(err) throw err;
+
+    client.on('connection', function(socket) {
+
+        var col = db.collection('messages');
+            sendStatus = function(s) {
+                socket.emit('status', s);
+            };
+        // Emit all messages
+        col.find().limit(100).sort({_id: 1}).toArray(function(err, res) {
+            if(err) throw err;
+            socket.emit('output', res);
+        });
+
+        // Wait for input
+        socket.on('input', function(data) {
+            var name = data.name,
+                message = data.message,
+                whitespacePattern = /^\s*$/;
+
+            if(whitespacePattern.test(name) || whitespacePattern.test(message)) {
+                sendStatus('Name and message is required.')
+            } else {
+                col.insert({ name: name, message: message }, function() {
+
+                    // Emit latest message to All clients
+                    client.emit('output', [data]);
+
+                    sendStatus({
+                        message: "Message sent",
+                        clear: true
+                    });
+                });
+            }
+        });
+    });
+});
+
+
+
